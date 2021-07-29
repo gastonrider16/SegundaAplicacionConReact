cdc8f86f869dc89f88206ed70d94c2e734cfdb61
gastonrider16 <48958097+gastonrider16@users.noreply.github.com>
2021-07-29 13:48:43 -0300

    Version 1 MoviesGrid.jsx

---
+++ MoviesGrid.jsx
@@ -0,0 +1,10 @@
+import movies from './movies.json';
+export function MoviesGrid() {
+    console.log(movies);
+    return(
+    <ul>
+        <li>Peli1</li>
+        <li>Peli2</li>
+    </ul>
+    );
+}
