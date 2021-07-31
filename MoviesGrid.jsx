47c8c96248782a0a09f3680e636b1c6739d59ec5
gastonrider16 <48958097+gastonrider16@users.noreply.github.com>
2021-07-30 21:36:22 -0300

    Version 1.4 Importando CSS NO GLOBAL

--- MoviesGrid.jsx
+++ MoviesGrid.jsx
@@ -1,8 +1,9 @@
 import movies from './movies.json';
 import {MovieCard} from './MovieCard';
+import styles from './MoviesGrid.module.css';
 export function MoviesGrid() {
     return(
-    <ul>
+    <ul className={styles.moviesGrid}>
         {movies.map( //movies es un arreglo
             (movie) => ( //=> ARROW FUNCTION
                <MovieCard key={movie.id} movie={movie}/>  //AGREGO KEY PARA MEJOR RENDIMIENTO ASI EVITO RENDERIZAR CONSTANTEMENTE
